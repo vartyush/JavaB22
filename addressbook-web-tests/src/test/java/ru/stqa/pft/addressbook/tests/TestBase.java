299b25015f88edac115d6f35632c050b715764c4
Viktoria Rubanova <viktoria.artyushina@t-systems.com>
2021-02-23 20:36:05 +0300

    Task7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,9 +3,10 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.*;
 
-public class TestBase extends ApplicationManager{
-    protected final ApplicationManager app = new ApplicationManager();
+public class TestBase {
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp()  {
